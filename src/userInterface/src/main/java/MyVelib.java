8b98679d60411971080960182199d225aaf27261
Osman Monla <osman.monla@student-cs.fr>
2023-05-30 21:12:03 +0200

    MyVelib

---
+++ src/userInterface/src/main/java/MyVelib.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import fr.cs.GroupNN.myVelib.*;
+
+import java.util.ArrayList;
+
+public class MyVelib {
+    // fields
+    private String name;
+    private ArrayList<User> users = new ArrayList<User>();
+    private ArrayList<DockingStation> dockingStationsList = new ArrayList<DockingStation>();
+
+}
