874917c07b0cbcf7b05c7698a179621e67a90212
Osman Monla <osman.monla@student-cs.fr>
2023-05-30 21:24:29 +0200

    MyVelib complete

--- src/userInterface/src/main/java/MyVelib.java
+++ src/userInterface/src/main/java/MyVelib.java
@@ -10,4 +10,45 @@ public class MyVelib {
     private ArrayList<User> users = new ArrayList<User>();
     private ArrayList<DockingStation> dockingStationsList = new ArrayList<DockingStation>();
 
+    // static field
+    private static ArrayList<MyVelib> myVelibNetworks = new ArrayList<MyVelib>();
+
+    // constructor
+    public MyVelib(String name){
+        super();
+        this.setName(name);
+        myVelibNetworks.add(this);
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public ArrayList<DockingStation> getDockingStationsList() {
+        return dockingStationsList;
+    }
+
+    public static ArrayList<MyVelib> getMyVelibNetworks() {
+        return myVelibNetworks;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // adding methods
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public void addDockingStation(DockingStation dockingStation){
+        dockingStationsList.add(dockingStation);
+    }
+
 }
